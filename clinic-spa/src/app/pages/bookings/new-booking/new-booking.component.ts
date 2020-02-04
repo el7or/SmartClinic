e797b761d5d98600dd2e442b7acf627f107c83f5
Ahmed Elhor <zizooo.elhor@gmail.com>
2020-02-04 09:59:28 +0200

    validation on time booking duplicate

--- clinic-spa/src/app/pages/bookings/new-booking/new-booking.component.ts
+++ clinic-spa/src/app/pages/bookings/new-booking/new-booking.component.ts
@@ -21,16 +21,30 @@ export class NewBookingComponent implements OnInit {
     protected dialogRef: NbDialogRef<NewBookingComponent>,
     private localeService: BsLocaleService
   ) {
+    // =====> localize datepicker:
     this.localeService.use(localStorage.getItem("langg"));
   }
 
   ngOnInit() {}
 
+  // =====> on choose booking date will fill table with all bookings in same day:
   onChooseBookingDate(input: NgForm) {
     //console.log(input);
     this.ChoosenDate = input.value;
   }
 
+  // =====> check if choosen booking time is already taken in same date:
+  onChooseBookingTime(input: NgForm){
+    //console.log(input);
+    if (new Date(input.value).getHours()==5&&new Date(input.value).getMinutes()==0) {
+      input.control.setErrors({'duplicateTime':true})
+    }
+    else{
+      input.control.setErrors(null)
+    }
+  }
+
+  // =====> on submit new booking:
   onAddBooking(form: NgForm) {
     this.doneSwal.fire();
     this.dialogRef.close();

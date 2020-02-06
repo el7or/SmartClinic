335dbedb09021a50c7fa133ca7f3d185ed5461c1
Ahmed Elhor <zizooo.elhor@gmail.com>
2020-02-06 08:57:36 +0200

    improve bookings today

--- clinic-spa/src/app/pages/bookings/bookings-today/bookings-today.component.ts
+++ clinic-spa/src/app/pages/bookings/bookings-today/bookings-today.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
-import { NbDialogService } from '@nebular/theme';
-import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  AfterViewInit,
+  ChangeDetectorRef
+} from "@angular/core";
+import { NbDialogService } from "@nebular/theme";
+import { SwalComponent } from "@sweetalert2/ngx-sweetalert2";
+import { CdkDragDrop, moveItemInArray } from "@angular/cdk/drag-drop";
 
-import { NewBookingComponent } from '../new-booking/new-booking.component';
+import { NewBookingComponent } from "../new-booking/new-booking.component";
 
 @Component({
-  selector: 'bookings-today',
-  templateUrl: './bookings-today.component.html',
-  styleUrls: ['./bookings-today.component.scss']
+  selector: "bookings-today",
+  templateUrl: "./bookings-today.component.html",
+  styleUrls: ["./bookings-today.component.scss"]
 })
-export class BookingsTodayComponent implements OnInit, AfterViewInit  {
+export class BookingsTodayComponent implements OnInit, AfterViewInit {
   @ViewChild("deleteSwal", { static: false }) deleteSwal: SwalComponent;
   @ViewChild("doneSwal", { static: false }) doneSwal: SwalComponent;
 
-  movies = [
+  /* movies = [
     'Episode I - The Phantom Menace',
     'Episode II - Attack of the Clones',
     'Episode III - Revenge of the Sith',
@@ -23,38 +29,102 @@ export class BookingsTodayComponent implements OnInit, AfterViewInit  {
     'Episode VI - Return of the Jedi',
     'Episode VII - The Force Awakens',
     'Episode VIII - The Last Jedi'
+  ]; */
+  bookingsList = [
+    {
+      seq: 1,
+      id: 125,
+      time: "05:00",
+      type: "Urgent Diagnose",
+      name: "محمد احمد السيد",
+      mobile: "021251021",
+      isEnter: true,
+      entryTime: "05:10",
+      isAttend: true,
+      attendTime: "04:45"
+    },
+    {
+      seq: 2,
+      id: 112,
+      time: "05:30",
+      type: "Diagnose",
+      name: "محمد علي محمد",
+      mobile: "0211425102",
+      isEnter: false,
+      entryTime: "05:30",
+      isAttend: true,
+      attendTime: "04:45"
+    },
+    {
+      seq: 3,
+      id: 325,
+      time: "06:00",
+      type: "Diagnose",
+      name: "عبير احمد علي",
+      mobile: "063546845",
+      isEnter: false,
+      entryTime: "06:09",
+      isAttend: true,
+      attendTime: "05:54"
+    },
+    {
+      seq: 4,
+      id: 222,
+      time: "07:00",
+      type: "Consult",
+      name: "عبد الفتاح عبد المتجلي عبد العزيز",
+      mobile: "015265566",
+      isEnter: false,
+      entryTime: "07:30",
+      isAttend: false,
+      attendTime: "07:13"
+    },
+    {
+      seq: 5,
+      id: 56,
+      time: "07:30",
+      type: "Consult",
+      name: "منى احمد السيد",
+      mobile: "012584686",
+      isEnter: false,
+      entryTime: "07:48",
+      isAttend: false,
+      attendTime: "07:20"
+    }
   ];
 
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
-  }
+  constructor(
+    private dialogService: NbDialogService,
+    private cd: ChangeDetectorRef
+  ) {}
 
-  constructor(private dialogService: NbDialogService,private cd: ChangeDetectorRef) { }
-
-  ngOnInit() {
-  }
-  ngAfterViewInit(){
+  ngOnInit() {}
+  ngAfterViewInit() {
     this.cd.detectChanges();
   }
 
-  onNewBook(){
+  onNewBook() {
     this.dialogService.open(NewBookingComponent, {
       context: {
         patientDetails: "محمد أحمد السيد"
       },
       autoFocus: true,
       hasBackdrop: true,
-      closeOnBackdropClick:false,
-      closeOnEsc:false
+      closeOnBackdropClick: false,
+      closeOnEsc: false
     });
   }
 
-  onDeleteBooking(){
+  onDeleteBooking() {
     this.deleteSwal.fire().then(result => {
       // =====> if click on add new booking:
       if (result.value) {
         this.doneSwal.fire();
       }
     });
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.bookingsList, event.previousIndex, event.currentIndex);
   }
 }

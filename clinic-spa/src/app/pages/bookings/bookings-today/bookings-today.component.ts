6b043b4ef1c8a628bf177521031d1f81544ee9ee
Ahmed Elhor <zizooo.elhor@gmail.com>
2020-02-06 10:22:01 +0200

    fix bug

--- clinic-spa/src/app/pages/bookings/bookings-today/bookings-today.component.ts
+++ clinic-spa/src/app/pages/bookings/bookings-today/bookings-today.component.ts
@@ -20,16 +20,6 @@ export class BookingsTodayComponent implements OnInit, AfterViewInit {
   @ViewChild("deleteSwal", { static: false }) deleteSwal: SwalComponent;
   @ViewChild("doneSwal", { static: false }) doneSwal: SwalComponent;
 
-  /* movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi'
-  ]; */
   bookingsList = [
     {
       seq: 1,

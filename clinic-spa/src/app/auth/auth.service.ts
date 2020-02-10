da84d4f615185f609d615c0489813efc27121ade
Ahmed Elhor <zizooo.elhor@gmail.com>
2020-02-10 11:22:00 +0200

    active login

--- clinic-spa/src/app/auth/auth.service.ts
+++ clinic-spa/src/app/auth/auth.service.ts
@@ -18,11 +18,11 @@ export class AuthService {
   }
 
   isAuthenticated():boolean{
-    return true;
-    /* if(this.currentUser.userName=='el7or' && this.currentUser.password == '123456'){
+    //return true;
+    if(this.currentUser.userName=='el7or' && this.currentUser.password == '123456'){
       return true;
     }
-    return false; */
+    return false;
   }
 
   logout(){

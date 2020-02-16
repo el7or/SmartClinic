e0c68cc652326c2523c84d5c26ddff0aedfb4517
Ahmed Elhor <zizooo.elhor@gmail.com>
2020-02-16 10:20:47 +0200

    analysis details

--- clinic-spa/src/app/pages/patients/patient-details/analysis/analysis-detail/analysis-detail.component.ts
+++ clinic-spa/src/app/pages/patients/patient-details/analysis/analysis-detail/analysis-detail.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
 import { NgForm } from '@angular/forms';
 import { NbDialogRef } from '@nebular/theme';
+import { FileUploader, FileItem } from 'ng2-file-upload';
+
+// const URL = '/api/';
+const URL = "https://api.imgur.com/3/upload";
 
 @Component({
   selector: 'analysis-detail',
@@ -11,9 +15,34 @@ import { NbDialogRef } from '@nebular/theme';
 export class AnalysisDetailComponent implements OnInit {
   @Input() analysisDetails: string;
   formLoading: boolean = false;
+  uploader: FileUploader;
+  response: string;
   @ViewChild("doneSwal", { static: false }) doneSwal: SwalComponent;
 
-  constructor(public dialogRef: NbDialogRef<AnalysisDetailComponent>) { }
+  constructor(public dialogRef: NbDialogRef<AnalysisDetailComponent>) {
+    this.uploader = new FileUploader({
+      url: URL,
+      disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
+      formatDataFunctionIsAsync: true,
+      formatDataFunction: async item => {
+        return new Promise((resolve, reject) => {
+          resolve({
+            name: item._file.name,
+            length: item._file.size,
+            contentType: item._file.type,
+            date: new Date()
+          });
+        });
+      }
+    });
+
+    this.uploader.onAfterAddingFile = (fileItem: FileItem) => {
+      let latestFile = this.uploader.queue[this.uploader.queue.length - 1];
+      this.uploader.queue = [];
+      this.uploader.queue.push(latestFile);
+      console.log(this.uploader.queue);
+    };
+  }
 
   ngOnInit() {
   }

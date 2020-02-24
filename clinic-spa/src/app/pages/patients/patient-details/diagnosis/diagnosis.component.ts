7b4e5fd6965a8c51cfd79a36ed8be7e0d55e9fdd
Ahmed Elhor <zizooo.elhor@gmail.com>
2020-02-24 15:22:32 +0200

    update diagnoses

--- clinic-spa/src/app/pages/patients/patient-details/diagnosis/diagnosis.component.ts
+++ clinic-spa/src/app/pages/patients/patient-details/diagnosis/diagnosis.component.ts
@@ -9,12 +9,41 @@ import { Location } from '@angular/common';
 })
 export class DiagnosisComponent implements OnInit {
   formLoading:boolean = false;
+  today:Date=new Date();
   @ViewChild("doneSwal", { static: false }) doneSwal: SwalComponent;
   @ViewChild("deleteSwal", { static: false }) deleteSwal: SwalComponent;
+  diagnosesNames = ['التشخيص 1','التشخيص 2','التشخيص 3'];
+  patientDiagnoses:any[] = [
+    {
+      name:"",
+      isNameValid:true,
+      grade:"",
+      note:""
+    }
+  ]
 
   constructor(public location:Location) { }
 
   ngOnInit() {
+  }
+
+  onAddNewItemToList(diagnoseName) {
+    this.diagnosesNames.push(diagnoseName);
+  }
+
+  // =====> on add new request to form from button:
+  onAddDiagnose() {
+    this.patientDiagnoses.push({
+      name:"",
+      isNameValid:true,
+      grade:"",
+      note:""
+    });
+  }
+
+  // =====> on remove row from requests:
+  onRemoveDiagnose(index) {
+    this.patientDiagnoses.splice(index, 1);
   }
 
   onDeleteDiagnosis(){

f224543afb2f7c57160861b28dbe2c1a7bfadc8c
Ahmed Elhor <zizooo.elhor@gmail.com>
2020-06-05 17:50:18 +0200

    try install new icons

--- clinic-spa/src/app/app.component.ts
+++ clinic-spa/src/app/app.component.ts
@@ -3,18 +3,21 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NbIconLibraries } from '@nebular/theme';
 
 @Component({
-  selector: 'ngx-app',
-  template: '<router-outlet></router-outlet>',
+  selector: "ngx-app",
+  template: "<router-outlet></router-outlet>",
 })
 export class AppComponent implements OnInit {
-
-  constructor() {
+  constructor(private iconLibraries: NbIconLibraries) {
+    this.iconLibraries.registerFontPack("fa", { iconClassPrefix: "fa" });
+    this.iconLibraries.registerFontPack("fas", { iconClassPrefix: "fas" });
+    this.iconLibraries.registerFontPack("fad", { iconClassPrefix: "fad" });
+    this.iconLibraries.registerFontPack("fal", { iconClassPrefix: "fal" });
+    this.iconLibraries.setDefaultPack("fad");
   }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 }

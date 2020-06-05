46eb18da605ed782272bf67ee5e83454fda8436f
Ahmed Elhor <zizooo.elhor@gmail.com>
2020-06-05 17:25:49 +0200

    add accounting to menu

--- clinic-spa/src/app/pages/pages-menu.ts
+++ clinic-spa/src/app/pages/pages-menu.ts
@@ -1,73 +1,91 @@
-import { NbMenuItem } from '@nebular/theme';
+import { NbMenuItem } from "@nebular/theme";
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'Current Bookings',
-    icon: 'calendar-outline',
-    link: '/pages/home',
+    title: "Current Bookings",
+    icon: "calendar-outline",
+    link: "/pages/home",
     home: true,
   },
   {
-    title: 'Patients Records',
-    icon: 'file-text-outline',
-    link: '/pages/patients',
-    pathMatch: "prefix"
+    title: "Patients Records",
+    icon: "file-text-outline",
+    link: "/pages/patients",
+    pathMatch: "prefix",
   },
   {
-    title: 'Today Bookings',
-    icon: 'clipboard-outline',
-    link: '/pages/bookings/today',
+    title: "Today Bookings",
+    icon: "clipboard-outline",
+    link: "/pages/bookings/today",
   },
   {
-    title: 'Browse Bookings',
-    icon: 'browser-outline',
-    link: '/pages/bookings/list',
-    pathMatch: "prefix"
+    title: "Browse Bookings",
+    icon: "browser-outline",
+    link: "/pages/bookings/list",
+    pathMatch: "prefix",
   },
   {
-    title: 'Chat',
-    icon: 'message-circle-outline',
-    link: '/pages/chat',
-    pathMatch: "full"
-  },
-  {
-    title: 'External Requests',
-    icon: 'corner-down-left-outline',
-    link: '/pages/externals',
-    pathMatch: "full"
-  },
-  {
-    title: 'Settings',
-    icon: 'settings-outline',
-    expanded:false,
-    data:'roleDoctor',
+    title: "Accounting",
+    icon: "activity-outline",
+    expanded: true,
+    data: "roleDoctor",
     children: [
       {
-        title: 'Clinic Setting',
-        link: '/pages/settings/clinic',
+        title: "Incomes",
+        icon: "trending-up-outline",
+        link: "/pages/accounting/income",
       },
       {
-        title: 'Patient Profile Setting',
-        link: '/pages/settings/patient',
+        title: "Expenses",
+        icon: "trending-down-outline",
+        link: "/pages/accounting/expense",
       },
       {
-        title: 'Users',
-        link: '/pages/settings/users',
-      }
+        title: "Profits",
+        icon: "award-outline",
+        link: "/pages/accounting/profit",
+      },
     ],
   },
   {
-    title: 'Contact Developer',
-    icon: 'info-outline',
-    link: '/pages/contact',
-    pathMatch: "full"
-  }
-  /* {
-    title: 'Payments',
-    icon: 'edit-2-outline',
-    link: '/pages/test',
+    title: "Chat",
+    icon: "message-circle-outline",
+    link: "/pages/chat",
+    pathMatch: "full",
   },
   {
+    title: "External Requests",
+    icon: "corner-down-left-outline",
+    link: "/pages/externals",
+    pathMatch: "full",
+  },
+  {
+    title: "Settings",
+    icon: "settings-outline",
+    expanded: false,
+    data: "roleDoctor",
+    children: [
+      {
+        title: "Clinic Setting",
+        link: "/pages/settings/clinic",
+      },
+      {
+        title: "Patient Profile Setting",
+        link: "/pages/settings/patient",
+      },
+      {
+        title: "Users",
+        link: "/pages/settings/users",
+      },
+    ],
+  },
+  {
+    title: "Contact Developer",
+    icon: "info-outline",
+    link: "/pages/contact",
+    pathMatch: "full",
+  },
+  /* {
     title: 'Reports',
     icon: 'monitor-outline',
     children: [
